3d965904c204212d7f5321eef08b57c97e9793bd
Rajat Gupta <rajatgupta431@gmail.com>
2015-04-28 15:00:15 +0530

    watch face showing rawbg with battery and delta

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -8,47 +8,47 @@ function DIRECTIONS(direction) {
 
         case "NONE":
             return 0;
-            break;
+            
         case "DoubleUp":
             return 1;
-            break;
+            
         case "SingleUp":
             return 2;
-            break;
+            
         case "FortyFiveUp":
             return 3;
-            break;
+            
         case "Flat":
             return 4;
-            break;
+            
         case "FortyFiveDown":
             return 5;
-            break;
+            
         case "SingleDown":
             return 6;
-            break;
+            
         case "DoubleDown":
             return 7;
-            break;
+            
         case 'NOT COMPUTABLE':
             return 8;
-            break;
+            
         case 'RATE OUT OF RANGE':
             return 9;
-            break;
+          
     }
-};
+}
 
 function fetchCgmData(lastReadTime, lastBG) {
 
     var response;
     var req = new XMLHttpRequest();
-    req.open('GET', "First Data Endpoint", true); //edit name below in message
+  req.open('GET', "TOP HALF DATA ENDPOINT ", true); //edit name below in message
     var req2 = new XMLHttpRequest();
-    req2.open('GET', "Second data endpoint", true); // edit name below in message 
+  req2.open('GET', "BOTTOM HALF DATA ENDPOINT", true); // edit name below in message 
 
     req.onload = function(e) {
-        console.log(req.readyState);
+      //  console.log(req.readyState);
         if (req.readyState == 4) {
             console.log(req.status);
             if (req.status == 200) {
@@ -61,16 +61,66 @@ function fetchCgmData(lastReadTime, lastBG) {
                     bgs = response.bgs,
                     currentBG = bgs[0].sgv,
                     currentDelta = bgs[0].bgdelta,
-                    currentTrend = DIRECTIONS(bgs[0].direction),
+                    currentTrend = DIRECTIONS(bgs[0].direction);
+              console.log("Current Delta:"+ currentDelta);
 
-                    delta = "Change: " + currentDelta + "mg/dL";
+                  //  delta = "Change: " + currentDelta + "mg/dL";
                 var battery = bgs[0].battery;
-                if (battery == undefined || battery == null)
+                if (battery === undefined || battery === null)
                     battery = "";
                 else battery = battery + "%";
 
                 if (parseInt(currentDelta) >= 0)
                     currentDelta = "+" + currentDelta;
+              var currentConvBG = currentBG;
+                var  specialValue = false;
+               var responsecals = response.cals;
+              //RAW BG
+             var currentCalcRaw = 0,
+                    //currentCalcRaw = 100000,
+                    formatCalcRaw = " ",
+                    currentRawFilt = bgs[0].filtered,
+                    formatRawFilt = " ",
+                    currentRawUnfilt = bgs[0].unfiltered,
+                    formatRawUnfilt = " ",
+                    currentNoise = bgs[0].noise,
+                    currentIntercept = "undefined",
+                    currentSlope = "undefined",
+                    currentScale = "undefined",
+                    currentRatio = 0;
+              
+              if (responsecals && responsecals.length > 0) {
+                      currentIntercept = responsecals[0].intercept;
+                      currentSlope = responsecals[0].slope;
+                      currentScale = responsecals[0].scale;
+                    }
+              
+              if (currentBG < 30) { specialValue = true; }
+                 // assign calculated raw value if we can
+                    if ( (typeof currentIntercept != "undefined") && (currentIntercept != "null") ){
+                        if (specialValue) {
+                          // don't use ratio adjustment
+                          currentCalcRaw = (currentScale * (currentRawUnfilt - currentIntercept) / currentSlope);
+                          //console.log("Special Value Calculated Raw: " + currentCalcRaw);
+                        } 
+                        else {
+                          currentRatio = (currentScale * (currentRawFilt - currentIntercept) / currentSlope / currentConvBG);
+                          currentCalcRaw = (currentScale * (currentRawUnfilt - currentIntercept) / currentSlope / currentRatio);
+                          //console.log("Current Ratio: " + currentRatio);
+                          //console.log("Normal BG Calculated Raw: " + currentCalcRaw);
+                        }          
+                    } // if currentIntercept                
+
+                    // assign raw sensor values if they exist
+                    if ( (typeof currentRawUnfilt != "undefined") && (currentRawUnfilt != "null") ) {
+                     
+                        formatRawFilt = ((Math.round(currentRawFilt / 1000)).toFixed(0));
+                        formatRawUnfilt = ((Math.round(currentRawUnfilt / 1000)).toFixed(0));
+                        formatCalcRaw = ((Math.round(currentCalcRaw)).toFixed(0));
+                        //console.log("Format Unfiltered: " + formatRawUnfilt);
+                      
+                     
+                    } // if currentRawUnfilt
 
 
                 if (currentBG < 55)
@@ -98,25 +148,77 @@ function fetchCgmData(lastReadTime, lastBG) {
 
 
                 req2.onload = function(o) {
+                  if(req2.readyState==4 && req2.status==200){
+                  
                     var response2 = JSON.parse(req2.responseText);
                     var battery2 = response2.bgs[0].battery;
-                    if (battery2 == undefined || battery2 == null)
+                    if (battery2 === undefined || battery2 === null)
                         battery2 = "";
                     else battery2 = battery2 + "%";
 
                     if (parseInt(response2.bgs[0].bgdelta) >= 0)
                         response2.bgs[0].bgdelta = "+" + response2.bgs[0].bgdelta;
+                    var currentBG2= response2.bgs[0].sgv;
+                      var currentConvBG2 = currentBG;
+                var  specialValue2 = false;
+                   var responsecals2 = response2.cals;
+                    
+                    
+                      //RAW BG
+             var currentCalcRaw2 = 0,
+                  //  currentCalcRaw2 = 100000,
+                   formatCalcRaw2 = " ",
+                    currentRawFilt2 = response2.bgs[0].filtered,
+                    formatRawFilt2 = " ",
+                    currentRawUnfilt2 = response2.bgs[0].unfiltered,
+                    formatRawUnfilt2 = " ",
+                    currentNoise2 = response2.bgs[0].noise,
+                    currentIntercept2 = "undefined",
+                    currentSlope2 = "undefined",
+                    currentScale2 = "undefined",
+                    currentRatio2 = 0;
+                                        if (responsecals2 && responsecals2.length > 0) {
+                      currentIntercept2 = responsecals2[0].intercept;
+                      currentSlope2 = responsecals2[0].slope;
+                      currentScale2 = responsecals2[0].scale;
+                    }
+              if (currentBG2 < 30) { specialValue2 = true; }
+                 // assign calculated raw value if we can
+                    if ( (typeof currentIntercept2 != "undefined") && (currentIntercept2 != "null") ){
+                        if (specialValue2) {
+                          // don't use ratio adjustment
+                          currentCalcRaw2 = (currentScale2 * (currentRawUnfilt2 - currentIntercept2) / currentSlope2);
+                          //console.log("Special Value Calculated Raw: " + currentCalcRaw2);
+                        } 
+                        else {
+                          currentRatio2 = (currentScale2 * (currentRawFilt2 - currentIntercept2) / currentSlope2 / currentConvBG2);
+                          currentCalcRaw2 = (currentScale2 * (currentRawUnfilt2 - currentIntercept2) / currentSlope2 / currentRatio2);
+                          //console.log("Current Ratio: " + currentRatio);
+                          //console.log("Normal BG Calculated Raw: " + currentCalcRaw);
+                        }          
+                    } // if currentIntercept                
+
+                    // assign raw sensor values if they exist
+                    if ( (typeof currentRawUnfilt2 != "undefined") && (currentRawUnfilt2 != "null") ) {
+                     
+                        formatRawFilt2 = ((Math.round(currentRawFilt2 / 1000)).toFixed(0));
+                        formatRawUnfilt2 = ((Math.round(currentRawUnfilt2 / 1000)).toFixed(0));
+                        formatCalcRaw2 = ((Math.round(currentCalcRaw2)).toFixed(0));
+                        //console.log("Format Unfiltered: " + formatRawUnfilt2);
+                      
+                     
+                    } // if currentRawUnfilt
 
                     var message = {
                         alert: alertValue,
                         bg: currentBG,
                         readtime: formatDate(new Date(bgs[0].datetime)),
                         icon: currentTrend,
-                        delta: battery + " FIRSTNAME " + currentDelta,
+                      delta: formatCalcRaw + " "+battery+" " + currentDelta,
                         icon2: DIRECTIONS(response2.bgs[0].direction),
                         bg2: response2.bgs[0].sgv,
                         readtime2: formatDate(new Date(response2.bgs[0].datetime)),
-                        delta2: battery2 + " SECONDNAME " + response2.bgs[0].bgdelta
+                        delta2: formatCalcRaw2 + " "+battery2+" " + response2.bgs[0].bgdelta
 
                     };
 
@@ -125,13 +227,17 @@ function fetchCgmData(lastReadTime, lastBG) {
 
 
 
-                };
+                }
+              
+            };
+              
 
                 req2.send(null);
 
 
             }
         }
+      
     };
     req.send(null);
 
